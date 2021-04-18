f41695c8f2d6637f0b155b66958555487fb2be89
fanyujie <1789239032@qq.com>
2021-04-18 23:55:36 +0800

    Create component_template.js

---
+++ component_template.js
@@ -0,0 +1,21 @@
+import {forwardRef, Fragment, ReactDOM, useCallback, useEffect, useImperativeHandle, useRef, useState} from "react"
+
+const Template=forwardRef(
+  ({...props},ref)=>
+  {
+    
+    useImperativeHandle(ref,()=>({
+         funname:()=>{},//.....functions
+                                 
+        }));
+    useEffect(
+      ()=>{
+        //....code
+      }
+     ,[/*...arg*/])//....object to be listened
+    
+    
+    return (...);
+  },
+  
+)
