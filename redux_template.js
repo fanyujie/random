368dd0cbcf54a6d82ff73c1a43f5833bc0c5721d
fanyujie <1789239032@qq.com>
2021-05-06 23:31:00 +0800

    Update redux_template.js

--- redux_template.js
+++ redux_template.js
@@ -2,16 +2,15 @@
 
 const singleredux =(state={/*...*/},action)=>
 {
-      let new_state=state;
        switch(action.type)
        {
            case:
            {
-              return new_state;
+              return {...state,/*...*/};
            }
          default:
            {
-             return new_state;
+             return state;
            }
        }
 }
@@ -32,4 +31,23 @@ const xxx_state =useSelector(state =>(state.xxx/*singleredux_name*/));
 
 const  xxx_dispatch = useDispatch();
 xxx_dispatch(/*your action*/); // (()=>({/*dict*/})) ()
+
+//redux-persist
+import {persistStore, persistReducer,persistCombineReducers} from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import storage from 'redux-persist/lib/storage'
+const reduce_config=
+{
+    key: 'root',
+    version: 0,
+    storage:storageSession,
+    // storage,
+    stateReconciler: autoMergeLevel2,
+    debug:true,
+    
+
+};
+const AllRedux = createStore(persistCombineReducers(reduce_config,
+    {LogRedux:LogRedux,}));
 
