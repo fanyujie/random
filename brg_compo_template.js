8190de074b09b1932890a0a14644667b0f99c98e
fanyujie <1789239032@qq.com>
2021-06-26 23:19:24 +0800

    Update brg_compo_template.js

--- brg_compo_template.js
+++ brg_compo_template.js
@@ -1,7 +1,7 @@
 const SurfaceCover = React.forwardRef(({...props}, ref) => {
   const refs=useRef()
   const table=[
-    { name:"",label:"",dict:[{"":0},{"":50},{"":50}] }
+    { name:"",label:"",dict:[{"":0},{"":50},{"":50}] },
   ]
   
   return (<Temp lable={""} name={""} table={table}
